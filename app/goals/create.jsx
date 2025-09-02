be2c378fa96768f0ee6572aee9d894667ba2ac1f
anjenneth <anjennethalcontin17@gmail.com>
2025-09-02 09:06:06 +0800

    Files Updated

--- app/goals/create.jsx
+++ app/goals/create.jsx
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
   },
   button: {
     padding: 18,
-    backgroundColor: '#21cc8d',
+    backgroundColor: '#1c7051ff',
     color: 'white',
     borderRadius: 8,
   }
---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBT8wDDFdPDtgEe9YBTtfVNafLd22rKHNM",
+  authDomain: "alcontin-mobile-app.firebaseapp.com",
+  projectId: "alcontin-mobile-app",
+  storageBucket: "alcontin-mobile-app.firebasestorage.app",
+  messagingSenderId: "756444183960",
+  appId: "1:756444183960:web:49c33a678f23514fc9ffed"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
